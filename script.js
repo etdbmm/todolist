9020be19c9cead4906d01b803fd375ba7859e5ca
etdbmm <etiennebom@gmail.com>
2024-10-05 22:17:09 -0300

    validação do campo

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function adicionarTarefa(){
-    let inputValue = document.querySelector("input").value
+    // Recebe o valor inserido no Input
+    let inputValue = document.querySelector("input").value.trim();
+
+    if (inputValue == '') {
+        alert("Por favor, insira uma tarefa.");
+        return;
+    }
 
     let li = document.createElement("li")
     li.innerHTML = inputValue + '<span onclick="deletarTarefa(this)">🗙</span>'
 
     document.querySelector("ul").appendChild(li)
 
+    // Limpa o campo do input após adicionar a tarefa
     document.querySelector("input").value = ''
 }
 
