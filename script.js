983a0743a06c3cfaf667bb185f9a81fdbdc979ae
etdbmm <etiennebom@gmail.com>
2024-10-07 00:01:26 -0300

    CSS Responsivo

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 function adicionarTarefa(){
+
+
     // Recebe o valor inserido no Input
     let inputValue = document.querySelector("input").value.trim();
 
+
+    // Campo vazio
     if (inputValue == '') {
         alert("Por favor, insira uma tarefa.");
         return;
     }
 
+    // Cria o item da lista
     let li = document.createElement("li")
     li.innerHTML = inputValue + '<span onclick="deletarTarefa(this)">🗙</span>'
-
     document.querySelector("ul").appendChild(li)
 
     // Limpa o campo do input após adicionar a tarefa
     document.querySelector("input").value = ''
 }
 
+// Deleta a tarefa
 function deletarTarefa(li){
     li.parentElement.remove()
 }
